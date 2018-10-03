e9e4e4e6c4f665720e65ed98d75b07fd0c94282e
Hughbert Kumwesiga <kumw1975@stthomas.edu>
2018-10-03 00:48:29 -0500

    'testing for all methods'

---
+++ TP__1/src/team__project__1__tests/DieTests.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package team__project__1__tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Time Trax
+ *
+ */
+public class DieTests {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#Die()}.
+	 */
+	@Test
+	public final void testDie() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#Die(int[])}.
+	 */
+	@Test
+	public final void testDieIntArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#getRollValue()}.
+	 */
+	@Test
+	public final void testGetRollValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#setRollValue(int)}.
+	 */
+	@Test
+	public final void testSetRollValue() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#roll()}.
+	 */
+	@Test
+	public final void testRoll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link team__project__1.Die#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
